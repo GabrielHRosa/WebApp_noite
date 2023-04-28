172d5834faf91c89b971dc4f216faea2eaed716b
GABRIEL ROSA <gabriel.rosa17@fatec.sp.gov.br>
2023-04-28 20:30:03 -0300

    interação entre back e front na parte dos produtos

--- WebApp_noite/Controllers/ProdutoController.cs
+++ WebApp_noite/Controllers/ProdutoController.cs
@@ -1,17 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp_noite.Models;
 
 namespace WebApp_noite.Controllers
 {
     public class ProdutoController : Controller
     {
+
+        public static List<ProdutosModel> db = new List<ProdutosModel>();
+
         public IActionResult Lista()
         {
-            return View();
+            return View(db);
         }
 
         public IActionResult Cadastrar()
         {
-            return View();
+            ProdutosModel model = new ProdutosModel();
+
+            return View(model);
+        }
+
+        public IActionResult SalvarDados(ProdutosModel produto)
+        {
+            if (produto.Id == 0)
+            {
+                Random rand = new Random();
+                produto.Id = rand.Next(1, 9999);
+
+                db.Add(produto);
+            }
+
+            return RedirectToAction("Lista");
         }
     }
 }
---
+++ WebApp_noite/Models/ProdutosModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApp_noite.Models
+{
+    public class ProdutosModel
+    {
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public int QtdEstoque { get; set; }
+
+
+    }
+}
