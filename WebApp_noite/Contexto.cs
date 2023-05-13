6bc9247b07f4a71617174216c9013dcca0052153
GABRIEL ROSA <gabriel.rosa17@fatec.sp.gov.br>
2023-05-12 21:08:01 -0300

    Adição class Contexto

---
+++ WebApp_noite/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApp_noite
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> opt)
+             :base(opt) { }
+
+    }
+}
